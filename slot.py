466b8173fb5bf08a11f4a2e0163ac54d53203137
Albert Biju <albertbiju13@gmail.com>
2025-06-03 13:50:53 +0530

    commit

--- slot.py
+++ slot.py
@@ -1,4 +1,8 @@
+
+
 MAX_LINES = 3
+MAX_BET= 100
+MIN_BET=1
 
 
 def deposit():
@@ -16,7 +20,7 @@ def deposit():
 
     return amount    
 
-def get_number_of_lines():
+def getLines():
     while (True):
         lines = input("Enter the number of lines(1-"+str(MAX_LINES)+"):")
         if(lines.isdigit()):
@@ -31,9 +35,33 @@ def get_number_of_lines():
 
     return lines 
 
+def getBet():
+    while (True):
+        bet = input(f"What is amount you want to bet on each line(${MIN_BET}-${MAX_BET})? $")
+        if(bet.isdigit()):
+            bet = int(bet)
+            if(MIN_BET <= bet <= MAX_BET):
+                break
+            else:
+                print("Please enter valid Bet Amount")
+
+        else:
+            print("Please Enter a valid Number.")
+
+    return bet 
+
 def main():
     balance = deposit()
-    lines=get_number_of_lines()
+    lines = getLines()
+    while True:
+        bet = getBet()
+        total_bet = lines * bet
+        if(total_bet>balance):
+            print(f"You can not bet since it exceeds your balance. Your current balance is ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines: ${total_bet}")
     
 
 main()
